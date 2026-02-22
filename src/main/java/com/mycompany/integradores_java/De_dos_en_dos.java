cf8c57af1caf2a9e663080d2d5637f3cdd60f05a
Andres Lopez <jgomezl@fi.uba.ar>
2026-02-21 21:03:34 -0300

    Ejercico imprimir numeros de a 2

---
+++ src/main/java/com/mycompany/integradores_java/De_dos_en_dos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.integradores_java;
+
+/**
+ *Realice un programa que muestre los numeros del 200 al 250 de a 2 en dos
+ * @author andres-lopez
+ */
+public class De_dos_en_dos {
+    public static void main (String [] args){
+        // Vamos a utilizar un for para y el incremento lo colocamos de a 2
+        for( int i = 200; i <= 250 ; i += 2 ){
+            System.out.println(i);
+        }
+    }
+}
